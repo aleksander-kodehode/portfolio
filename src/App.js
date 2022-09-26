72ecd944d999eebea2f20b96010a142dbfb10936
aleksander-kodehode <106583899+aleksander-kodehode@users.noreply.github.com>
2022-09-26 20:45:35 +0200

    page setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import theme from "./theme";
+import NavBar from "./components/NavBar";
+import AboutMePage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
+import ProjectsPage from "./pages/ProjectsSection";
+import { ThemeProvider } from "styled-components";
+
 const App = () => {
   return (
-    <div>
-      <p>test</p>
-    </div>
+    <ThemeProvider theme={theme}>
+      <NavBar />
+      <HomePage />
+      <AboutMePage />
+      <ProjectsPage />
+      <ContactPage />
+    </ThemeProvider>
   );
 };
 
