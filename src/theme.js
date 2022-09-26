4f756fda67d762c092f20782f2b3d9cc39c6656c
aleksander-kodehode <106583899+aleksander-kodehode@users.noreply.github.com>
2022-09-26 20:45:20 +0200

    theme setup and test, setup needed for dark mode switch

---
+++ src/theme.js
@@ -0,0 +1,22 @@
+const theme = {
+  darkTheme: {
+    dark: "#16161A",
+    darkGrey: "#242629",
+    lightGrey: "#94A1B2",
+    white: "#FFFFFE",
+    purple: "#7F5AF0",
+    green: "#2CB67D",
+  },
+  lightTheme: {},
+  fonts: {
+    family: ["Roboto", "sans-serif"],
+    fontSizes: {
+      small: "0.625rem",
+      medium: "1rem",
+      large: "2rem",
+      xl: "3rem",
+    },
+  },
+};
+
+export default theme;
