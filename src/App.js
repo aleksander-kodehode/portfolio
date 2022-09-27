c0bc63ea37a2dfb8d7a1077d296d1663bb75a85d
aleksander-kodehode <106583899+aleksander-kodehode@users.noreply.github.com>
2022-09-27 11:00:19 +0200

    moved ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import theme from "./theme";
 import NavBar from "./components/NavBar";
 import AboutMePage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 import HomePage from "./pages/HomePage";
 import ProjectsPage from "./pages/ProjectsSection";
-import { ThemeProvider } from "styled-components";
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <NavBar />
       <HomePage />
       <AboutMePage />
       <ProjectsPage />
       <ContactPage />
-    </ThemeProvider>
+    </>
   );
 };
 
