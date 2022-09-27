06449884d9db764fdc094c5cc8de5139d87b4fbd
aleksander-kodehode <106583899+aleksander-kodehode@users.noreply.github.com>
2022-09-27 14:40:04 +0200

    Section heading

---
+++ src/componentsStyled/typographyStyled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const SectionHeading = styled.h1`
+  font-size: ${(props) => props.theme.fonts.fontSizes.large};
+  font-weight: 700;
+  text-align: center;
+  display: block;
+  width: 100%;
+  margin-top: 0.625rem;
+  margin-bottom: 1rem;
+`;
