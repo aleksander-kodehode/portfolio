47092de3842f28adebdc2b678b49ae73937ef092
aleksander-kodehode <106583899+aleksander-kodehode@users.noreply.github.com>
2022-09-28 23:41:59 +0200

    added project obj

--- src/pages/text/textContent.js
+++ src/pages/text/textContent.js
@@ -1,5 +1,8 @@
 import vectorTopRight from "../../img/vectorTopRight.png";
 import vectorBottomRight from "../../img/vectorBottomRight.svg";
+import pokeDex from "../../img/pokeDex.png";
+import pswdGen from "../../img/pswdGen.png";
+import todoList from "../../img/todoList.png";
 
 export const TEXT_AND_CONTENT = {
   NAVIGATION_BAR: {
@@ -16,11 +19,73 @@ export const TEXT_AND_CONTENT = {
   },
   PROJECTS_PAGE: {
     HEADING_PAGE: "",
-    HEADING_PROJECT: "",
-    PROJECT_DESCRIPTION: "",
-    BUTTON_DEMO: "",
-    BUTTON_GITHUB: "",
-    IMAGE: "",
+    PROJECT: [
+      {
+        NAME: "PokeDex",
+        HEADING: "PokeDex",
+        DESCRIPTION:
+          "Browse pokemon based on generations or search for one you would like to see. Click on a pokemon to get more stats of that pokemon. Uses API to fetch data based on parameters, built using Javascript, HTML & CSS",
+        URL_REPOSITORY: "https://github.com/aleksander-kodehode/pokeDex",
+        URL_LIVE: "https://aleksander-kodehode.github.io/pokeDex/",
+        SCREENSHOT: pokeDex,
+        ID: 1,
+      },
+      {
+        NAME: "PasswordGenerator",
+        HEADING: "Password Generator",
+        DESCRIPTION:
+          "Simple password generator that allows you to set certain parameters of how you want the password to be, such as password length, symbols and numbers. With a copy to clipboard feature. Built using Javascript, HTML & CSS",
+        URL_REPOSITORY:
+          "https://github.com/aleksander-kodehode/password-generator/",
+        URL_LIVE: "https://aleksander-kodehode.github.io/password-generator/",
+        SCREENSHOT: pswdGen,
+        ID: 2,
+      },
+      {
+        NAME: "TodoList",
+        HEADING: "Todo List",
+        DESCRIPTION:
+          "Todo list that stores tasks in localStorage. Built using Javascript, HTML & CSS",
+        URL_REPOSITORY: "https://github.com/aleksander-kodehode/todoList",
+        URL_LIVE: "https://aleksander-kodehode.github.io/todoList/",
+        SCREENSHOT: todoList,
+        ID: 3,
+      },
+      {
+        NAME: "PokeDex",
+        HEADING: "PokeDex",
+        DESCRIPTION:
+          "Browse pokemon based on generations or search for one you would like to see. Click on a pokemon to get more stats of that pokemon. Uses API to fetch data based on parameters, built using Javascript, HTML & CSS",
+        URL_REPOSITORY: "https://github.com/aleksander-kodehode/pokeDex",
+        URL_LIVE: "https://aleksander-kodehode.github.io/pokeDex/",
+        SCREENSHOT:
+          "https://github.com/aleksander-kodehode/pokeDex/blob/master/img/preview.png",
+        ID: 1,
+      },
+      {
+        NAME: "PasswordGenerator",
+        HEADING: "Password Generator",
+        DESCRIPTION:
+          "Simple password generator that allows you to set certain parameters of how you want the password to be, such as password length, symbols and numbers. With a copy to clipboard feature. Built using Javascript, HTML & CSS",
+        URL_REPOSITORY:
+          "https://github.com/aleksander-kodehode/password-generator/",
+        URL_LIVE: "https://aleksander-kodehode.github.io/password-generator/",
+        SCREENSHOT:
+          "https://github.com/aleksander-kodehode/pokeDex/blob/master/img/preview.png",
+        ID: 2,
+      },
+      {
+        NAME: "TodoList",
+        HEADING: "Todo List",
+        DESCRIPTION:
+          "Todo list that stores tasks in localStorage. Built using Javascript, HTML & CSS",
+        URL_REPOSITORY: "https://github.com/aleksander-kodehode/todoList",
+        URL_LIVE: "https://aleksander-kodehode.github.io/todoList/",
+        SCREENSHOT:
+          "https://github.com/aleksander-kodehode/pokeDex/blob/master/img/preview.png",
+        ID: 3,
+      },
+    ],
   },
   CONTACT_PAGE: {
     HEADING_PAGE: "",
