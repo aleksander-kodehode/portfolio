b87e01c5f0df592a78a7e8f0bca013768c5434c1
aleksander-kodehode <106583899+aleksander-kodehode@users.noreply.github.com>
2022-09-28 23:39:29 +0200

    fix for centering navItems

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,49 +1,57 @@
 import { Nav, NavList, NavItems } from "../componentsStyled/navStyles";
 import { TEXT_AND_CONTENT } from "../pages/text/textContent";
 import Switch from "./Switch";
-
+import { navHeight } from "../componentsStyled/navStyles";
 const NavBar = () => {
   const NAVIGATION_BAR = TEXT_AND_CONTENT.NAVIGATION_BAR;
 
   return (
     <Nav>
       <NavList>
-        <NavItems
-          to="home"
-          spy={true}
-          activeClass="nav-bar-link-active"
-          smooth={true}
-          offset={-100}
-        >
-          {NAVIGATION_BAR.HOME}
-        </NavItems>
-        <NavItems
-          to="about-me"
-          spy={true}
-          activeClass="nav-bar-link-active"
-          smooth={true}
-          offset={-100}
-        >
-          {NAVIGATION_BAR.ABOUT_ME}
-        </NavItems>
-        <NavItems
-          to="projects"
-          spy={true}
-          activeClass="nav-bar-link-active"
-          smooth={true}
-          offset={-100}
-        >
-          {NAVIGATION_BAR.PROJECTS}
-        </NavItems>
-        <NavItems
-          to="contact"
-          spy={true}
-          activeClass="nav-bar-link-active"
-          smooth={true}
-          offset={-100}
-        >
-          {NAVIGATION_BAR.CONTACT}
-        </NavItems>
+        <li>
+          <NavItems
+            to="home"
+            spy={true}
+            activeClass="nav-bar-link-active"
+            smooth={true}
+            offset={-navHeight}
+          >
+            {NAVIGATION_BAR.HOME}
+          </NavItems>
+        </li>
+        <li>
+          <NavItems
+            to="about-me"
+            spy={true}
+            activeClass="nav-bar-link-active"
+            smooth={true}
+            offset={-navHeight}
+          >
+            {NAVIGATION_BAR.ABOUT_ME}
+          </NavItems>
+        </li>
+        <li>
+          <NavItems
+            to="projects"
+            spy={true}
+            activeClass="nav-bar-link-active"
+            smooth={true}
+            offset={-navHeight}
+          >
+            {NAVIGATION_BAR.PROJECTS}
+          </NavItems>
+        </li>
+        <li>
+          <NavItems
+            to="contact"
+            spy={true}
+            activeClass="nav-bar-link-active"
+            smooth={true}
+            offset={-navHeight}
+          >
+            {NAVIGATION_BAR.CONTACT}
+          </NavItems>
+        </li>
       </NavList>
 
       <Switch />
