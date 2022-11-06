5ae19ce4ecee2f9badea2ebd266e0d5469fabd96
alekhoff <alekhoff@gmail.com>
2022-11-06 13:51:17 +0100

    hyperlink styling

--- src/componentsStyled/typographyStyled.js
+++ src/componentsStyled/typographyStyled.js
@@ -9,3 +9,14 @@ export const SectionHeading = styled.h1`
   margin-top: 0.625rem;
   margin-bottom: 3rem;
 `;
+
+export const Hyperlink = styled.a`
+  color: ${(props) => props.theme.hyperlink};
+  font-size: 2rem;
+  text-decoration: none;
+  transition: filter 200ms ease-in-out;
+  &:hover,
+  :active {
+    filter: brightness(130%);
+  }
+`;
