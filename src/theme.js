e176dbb146f0ebf1ef9867f022bf1a5cad22c611
alekhoff <alekhoff@gmail.com>
2022-11-06 13:47:15 +0100

    theme changes

--- src/theme.js
+++ src/theme.js
@@ -2,10 +2,13 @@ export const darkTheme = {
   mainBackground: "#16161A",
   secondaryBackground: "#242629",
   lightGrey: "#94A1B2",
+  heading: "#FFFFFE",
+  paragraph: "#94A1B2",
   white: "#FFFFFE",
   purple: "#7F5AF0",
   green: "#2CB67D",
   switch: "#28292c",
+  hyperlink: "#7F5AF0",
   fonts: {
     family: ["Roboto", "sans-serif"],
     fontSizes: {
@@ -17,13 +20,17 @@ export const darkTheme = {
   },
 };
 export const lightTheme = {
-  mainBackground: "#fff",
-  secondaryBackground: "#eee",
+  mainBackground: "#FFF",
+  secondaryBackground: "#bfc2ff",
   lightGrey: "#94A1B2",
+  heading: "#000",
+  paragraph: "#535353",
   white: "#FFFFFE",
   purple: "#7F5AF0",
-  green: "#2CB67D",
-  switch: "#d8dbe0",
+  /* TODO: Refactor names */
+  green: "#7f5af0",
+  switch: "#28292c",
+  hyperlink: "#7F5AF0",
   fonts: {
     family: ["Roboto", "sans-serif"],
     fontSizes: {
