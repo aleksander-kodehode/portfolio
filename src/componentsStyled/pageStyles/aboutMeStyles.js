3f6728404b1f6c6fa7ca9e9f954951696fdf5b3b
alekhoff <alekhoff@gmail.com>
2022-11-06 13:51:09 +0100

    aboutView styles

---
+++ src/componentsStyled/pageStyles/aboutMeStyles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const AboutWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 85%;
+  max-width: 1400px;
+  margin: 0 auto;
+`;
+export const TextContentWrapper = styled.div``;
+export const SkillWrapper = styled.div`
+  & > h2 {
+    text-align: center;
+  }
+`;
