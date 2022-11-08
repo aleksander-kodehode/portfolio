a71362d9fae5d6317535a2c9b2273986787c27ff
aleksander-kodehode <aleksander.kodehode@gmail.com>
2022-11-08 13:59:34 +0100

    i18n config

--- src/services/i18n.js
+++ src/services/i18n.js
@@ -1,18 +1,35 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
-import HttpApi from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
+import translationEn from "../locales/en/translation.json";
+import translationNb from "../locales/nb/translation.json";
+
+const resources = {
+  en: {
+    translation: translationEn,
+  },
+  nb: {
+    translation: translationNb,
+  },
+};
 
 i18next
-  .use(initReactI18next)
-  .use(HttpApi)
   .use(LanguageDetector)
+  .use(initReactI18next)
   .init({
-    lng: "en",
-    fallbackLng: "en",
+    resources,
     supportedLngs: ["en", "nb"],
+    nonExplicitSupportedLngs: true,
+    fallbackLng: "en",
+    keySeparator: ".",
     interpolation: {
       escapeValue: false,
+    },
+    backend: {
+      loadPath: "../publ/locales/{{lng}}/{{ns}}",
+      parse: function (data) {
+        console.log(data);
+      },
     },
     //TODO: Disable before production build
     debug: true,
