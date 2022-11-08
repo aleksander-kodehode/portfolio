0f405b196db554a9ec590c7da25ccd9b1cd46d57
aleksander-kodehode <aleksander.kodehode@gmail.com>
2022-11-08 13:58:54 +0100

    added a switcher for languages

--- src/components/LanguageSwitcher.js
+++ src/components/LanguageSwitcher.js
@@ -1,16 +1,40 @@
 import { useTranslation } from "react-i18next";
+import styled from "styled-components";
+
+const LangSelectContainer = styled.div`
+  margin-right: auto;
+`;
+const LangSwitchBtn = styled.button`
+  margin-left: 20px;
+  border: none;
+  background: transparent;
+  color: ${(props) => props.theme.purple};
+  font-weight: 700;
+  font-size: 18px;
+  cursor: pointer;
+  transition: all 250ms ease-in-out;
+`;
+
 const LanguageSwitcher = () => {
   const { i18n } = useTranslation();
   return (
-    <div className="select">
-      <select
+    <LangSelectContainer>
+      <LangSwitchBtn
+        value={i18n.language}
+        onClick={() => {
+          i18n.changeLanguage(i18n.language === "nb" ? "en" : "nb");
+        }}
+      >
+        {i18n.language === "nb" ? "English" : "Norsk"}
+      </LangSwitchBtn>
+      {/* <select
         value={i18n.language}
         onChange={(e) => i18n.changeLanguage(e.target.value)}
       >
         <option value="en">English</option>
         <option value="nb">Norsk</option>
-      </select>
-    </div>
+      </select> */}
+    </LangSelectContainer>
   );
 };
 export default LanguageSwitcher;
