01f0ef2feeb5c3a18a1fafe20ce3029248396f10
aleksander-kodehode <aleksander.kodehode@gmail.com>
2022-11-16 20:51:01 +0100

    simplifies darkmode button

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,25 +1,56 @@
 import { useContext } from "react";
 import { themeContext } from "../context/themeContext";
-import "./Switch.css";
+import { Icon } from "@iconify/react";
+import styled from "styled-components";
+import { device } from "../componentsStyled/mediaQueriesBreakPoints";
+
+const Button = styled.button`
+  margin-right: 30px;
+  margin-left: 30px;
+  background: none;
+  border: none;
+  border-radius: 50%;
+  text-transform: none;
+  box-shadow: none;
+  cursor: pointer;
+  padding: 3px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all cubic-bezier(0.445, 0.05, 0.55, 0.95) 250ms;
+  @media ${device.tabletS} {
+    margin: 0 2.5rem 0 1rem;
+  }
+  &:hover,
+  :active {
+    & :first-child {
+      color: ${(props) => props.theme.purple};
+    }
+  }
+  & :first-child {
+    color: white;
+    width: 24px;
+    height: 24px;
+    transition: all cubic-bezier(0.445, 0.05, 0.55, 0.95) 250ms;
+  }
+`;
+
 const Switch = () => {
   const contextState = useContext(themeContext);
 
   const onToggle = () => {
     //send state of toggle btn to the context
     contextState.setIsToggled(!contextState.isToggled);
-    if (!contextState.isToggled) {
-    }
   };
 
-  return (
-    <label className="toggle-switch">
-      <input
-        type="checkbox"
-        checked={contextState.isToggled}
-        onChange={onToggle}
-      />
-      <span className="switch" />
-    </label>
+  return contextState.isToggled ? (
+    <Button onClick={onToggle}>
+      <Icon icon="ph:sun-duotone" />
+    </Button>
+  ) : (
+    <Button onClick={onToggle}>
+      <Icon icon="ph:moon-duotone" />
+    </Button>
   );
 };
 
